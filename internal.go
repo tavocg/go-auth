4b4cc5ece4a6db6446bfe709cb60622c541d2200
tavocg <gustavo.calvo.cr@gmail.com>
2026-05-10 13:29:35 -0600

    hmm

--- internal.go
+++ internal.go
@@ -1,7 +1,5 @@
 package auth
 
-// TODO: remove unused errors, merge if too similar or unnecessary distinction
-
 type errStr string
 
 func (e errStr) Error() string {
@@ -11,12 +9,12 @@ func (e errStr) Error() string {
 var (
 	// ErrInvalidIdentity reports that the supplied identity cannot be used.
 	ErrInvalidIdentity error = errStr("invalid identity")
+	// ErrInvalidSecret reports that a signing secret is missing or unusable.
+	ErrInvalidSecret error = errStr("invalid secret")
 	// ErrInvalidToken reports that a token cannot be parsed or trusted.
 	ErrInvalidToken error = errStr("invalid token")
 	// ErrExpiredToken reports that a token is no longer valid because it expired.
 	ErrExpiredToken error = errStr("expired token")
 	// ErrRevokedToken reports that a token has already been revoked.
 	ErrRevokedToken error = errStr("revoked token")
-	// ErrUnsupportedToken reports that a token format or algorithm is not supported.
-	ErrUnsupportedToken error = errStr("unsupported token")
 )
